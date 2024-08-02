d071e616640d049d7aae86ee3ef9f93eb5d73924
João Périco <jeperico@terra.com.br>
2024-08-02 17:58:52 -0300

    init

---
+++ eptran/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EptranConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "eptran"
---
+++ eptran/settings.py
@@ -0,0 +1,13 @@
+
+INSTALLED_APPS = [
+  "jazzmin",
+  "django.contrib.admin",
+  "rest_framework",
+  "rest_framework.swagger",
+]
+
+JAZZMIN_SETTINGS = {
+  "site_title": "Eptran API Admin",
+  "site_header": "Eptran API",
+  "welcome_sign": "Welcome to the Eptran API Admin",
+}
